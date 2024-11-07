b1a264f05871405a3b2b9b649cbe664150b3b599
= <jacob.swindell@btinternet.com>
2024-11-07 18:22:40 +0000

    add object-based movement

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,16 +1,22 @@
 extends CSGSphere3D
 
-var target_position: Vector3
+@export var target_node: Node3D
 var lerp_weight = 0.1
+var moving: bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	target_position = position
 	SignalBus.connect("move_player_to", move)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	position = position.lerp(target_position, lerp_weight)
+	if moving:
+		position = position.lerp(target_node.global_position + Vector3(0, 0, 0.8), lerp_weight)
+		if position.distance_to(target_node.global_position + Vector3(0, 0, 0.8)) < 0.17:
+			moving = false
+	else:
+		position = target_node.global_position + Vector3(0, 0, 0.8)
 
 func move(object: Node3D):
-	target_position = object.position + Vector3(0, 0, 0.8)
+	target_node = object
+	moving = true
