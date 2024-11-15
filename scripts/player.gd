23857afb2b7d7a385f088694987b976d16b6fea1
= <jacob.swindell@btinternet.com>
2024-11-15 16:41:18 +0000

    change ninja model + dash effect + caught animation

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,8 +1,10 @@
 extends Node3D
 
 @export var target_node: Node3D
+@export var camera_node: Camera3D
 
 @onready var alert_sound = $AudioStreamPlayer
+@onready var dash_particles: GPUParticles3D = $DashParticles
 
 var lerp_weight = 0.1
 var moving: bool = false
@@ -11,13 +13,14 @@ var visibility = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$AnimationPlayer.play("Hiding")
+	$AnimationPlayer.play("hide") # Hiding
 	SignalBus.connect("move_player_to", set_move_target)
 	SignalBus.connect("seen_by", change_visiblity)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if caught:
+		look_at(Vector3(camera_node.global_position.x, 0, camera_node.global_position.z), Vector3.UP)
 		return
 	if moving:
 		check_if_caught()
@@ -29,7 +32,8 @@ func _process(delta: float) -> void:
 func set_move_target(object: Node3D):
 	target_node = object
 	moving = true
-	$AnimationPlayer.play("Sneaking")
+	dash_particles.emitting = true
+	#$AnimationPlayer.play("Sneaking")
 
 func change_visiblity(amount: int):
 	visibility += amount
@@ -38,7 +42,10 @@ func check_if_caught():
 	if visibility > 0:
 		moving = false
 		caught = true
+		dash_particles.emitting = false
 		alert_sound.play()
+		$AnimationPlayer.play("caught")
+		return
 
 func move():
 	position = position.lerp(target_node.global_position + Vector3(0, 0, 0.8), lerp_weight)
@@ -48,5 +55,6 @@ func move():
 func check_target_reached():
 	if position.distance_to(target_node.global_position + Vector3(0, 0, 0.8)) < 0.17:
 		moving = false
-		$AnimationPlayer.play("Hiding")
+		$AnimationPlayer.play("hide") # Hiding
+		dash_particles.emitting = false
 		rotation = Vector3.ZERO
