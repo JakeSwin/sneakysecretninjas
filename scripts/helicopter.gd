053fa9d88db759a152bb0067e7af89d45eb4178a
= <jacob.swindell@btinternet.com>
2024-11-29 17:36:41 +0000

    helicopter light + better player movement

--- scripts/helicopter.gd
+++ scripts/helicopter.gd
@@ -1,11 +1,11 @@
 extends Area3D
 
+@onready var ninjav_2: Node3D = $"../../ninjav2"
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+func _on_area_entered(area: Area3D) -> void:
+	if area.name == "NinjaArea":
+		SignalBus.emit_signal("caught", ninjav_2)
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _on_timer_timeout() -> void:
+	monitoring = true
+	monitorable = true
--- scripts/player.gd
+++ scripts/player.gd
@@ -12,26 +12,34 @@ var moving: bool = false
 var caught: bool = false
 var visibility = 0
 var ninja: Node
-
+var speed = 60
+var dist = 0
+var target: Vector3
+var time_to_location = 0
+var progress = 0
+var prev_pos: Vector3
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$AnimationPlayer.play("hide") # Hiding
 	SignalBus.connect("move_player_to", set_move_target)
 	SignalBus.connect("seen_by", change_visiblity)
+	SignalBus.connect("caught", get_caught)
 	ninja = get_tree().get_nodes_in_group("ninja")[0]
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	target = target_node.global_position + Vector3(0, 0, 1.5)
+	target.y = 0.3
 	if caught:
 		look_at(Vector3(camera_node.global_position.x, 0, camera_node.global_position.z), Vector3.UP)
 		return
 	if moving:
 		check_if_caught()
-		move()
+		move(delta)
 	else:
 		# Follow Bush Position
-		position = target_node.global_position + Vector3(0, 0, 0.8)
+		position = target
 
 	if Global.smoked == true:
 		var pos = smoke_particles.global_position
@@ -43,33 +51,41 @@ func _process(delta: float) -> void:
 	else:
 		$SmokeParticles.emitting = false
 	
-	
+
 func set_move_target(object: Node3D):
 	target_node = object
 	moving = true
 	dash_particles.emitting = true
-	#$AnimationPlayer.play("Sneaking")
+	progress = 0
+	prev_pos = position
+	dist = prev_pos.distance_to(object.global_position + Vector3(0, 0, 1.5))
+	time_to_location = dist / speed
+	target = target_node.global_position + Vector3(0, 0, 1.5)
+	target.y = 0.3
 
 func change_visiblity(amount: int):
 	visibility += amount
 
 func check_if_caught():
 	if visibility > 0 and Global.smoked == false:
-		moving = false
-		caught = true
-		dash_particles.emitting = false
-		alert_sound.play()
-		$AnimationPlayer.play("caught")
 		SignalBus.emit_signal("caught", self)
 		return
 
-func move():
-	position = position.lerp(target_node.global_position + Vector3(0, 0, 0.8), lerp_weight)
-	look_at(target_node.global_position + Vector3(0, 0, 0.8), Vector3.UP)
+func get_caught(_ninja):
+	moving = false
+	caught = true
+	dash_particles.emitting = false
+	alert_sound.play()
+	$AnimationPlayer.play("caught")
+
+func move(delta):
+	progress = progress + (delta / time_to_location)
+	position = lerp(prev_pos, target, progress)
+	look_at(target, Vector3.UP)
 	check_target_reached()
 
 func check_target_reached():
-	if position.distance_to(target_node.global_position + Vector3(0, 0, 0.8)) < 0.17:
+	if progress > 1:
 		moving = false
 		$AnimationPlayer.play("hide") # Hiding
 		dash_particles.emitting = false
